7e031ccefe32bd2b54d695c9c2fd7ff19228e29f
Sean Martin <seankieran.m@hotmail.com>
2021-02-25 17:23:08 +0000

    Initial commit of code

---
+++ 2021/Actual/ronaldo.py
@@ -0,0 +1,12 @@
+# TODO import your packages
+
+
+def ronaldo_solution(input_info, **kwargs):
+    # TODO write your solution here!
+
+    # TODO Parse any parameters you need
+    val1 = kwargs.get("name", None)
+
+    solution = 0
+    score = 0
+    return solution, score
